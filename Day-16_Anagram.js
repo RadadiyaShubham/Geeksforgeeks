4049100ffd3a0a4317e3d143ca62018e7076e112
Radadiya Shubham <radadiyashubham62@gmail.com>
2025-02-04 10:14:38 +0530

    Add files via upload

---
+++ Day-16_Anagram.js
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let a = readLine().trim();
+        let b = readLine().trim();
+        let obj = new Solution();
+        if (obj.areAnagrams(a, b))
+            console.log("true");
+        else
+            console.log("false");
+        console.log("~");
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @returns {boolean}
+ */
+
+class Solution {
+    
+    areAnagrams(s1, s2) {
+        if (s1.length !== s2.length) return false;
+        return s1.split('').sort().join('') === s2.split('').sort().join('');
+    }
+}
