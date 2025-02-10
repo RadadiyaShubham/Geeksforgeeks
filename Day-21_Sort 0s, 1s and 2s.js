3eb91f14edb4fc1e3058a173697b53f92dc6d69c
Radadiya Shubham <radadiyashubham62@gmail.com>
2025-02-10 10:01:43 +0530

    Add files via upload

---
+++ Day-21_Sort 0s, 1s and 2s.js
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => { inputString += inputStdin; });
+
+process.stdin.on("end", (_) => {
+    inputString =
+        inputString.trim().split("\n").map((string) => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+// Position this line where user code will be pasted.
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let arr = readLine().trim().split(" ").map((x) => parseInt(x));
+        let obj = new Solution();
+        obj.sort012(arr);
+        let S = "";
+        for (let j = 0; j < arr.length; j++) {
+            S += arr[j];
+            S += " "
+        }
+        console.log(S);
+        console.log("~");
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for javascript
+/**
+ * @param {number[]} a
+ * @returns {void}
+ */
+class Solution {
+
+    sort012(arr) {
+        return arr.sort();
+    }
+}
